1e8378c293e97c84ce8548ca0ee9c54e378666ce
Andy Wu <andyww09@gmail.com>
2023-10-09 15:02:18 -0700

    add html and templates for notes

--- notes_api/urls.py
+++ notes_api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path 
 
 from . import views 
-
+app_name = 'notes_api'
 urlpatterns = [
     path('notes/', views.NoteList.as_view(), name='note-list'),
     path('notes/<str:pk>/', views.NoteDetail.as_view(), name='note-detail'),
--- notes_api/views.py
+++ notes_api/views.py
@@ -1,15 +1,39 @@
 from django.shortcuts import render
-from rest_framework import generics
+# from rest_framework import generics
+from django.views import generic
 from .models import Note
 from .serializers import NoteSerializer
 
 # @permission_classes([IsAuthenticated])
-class NoteList(generics.ListCreateAPIView):
-    queryset = Note.objects.all().order_by('-updated')
-    serializer_class = NoteSerializer
+class NoteList(generic.ListView):
+    template_name = "notes_api/index.html"
+    context_object_name = "latest_notes_list"
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Note.objects.all().order_by('-updated')
+
+# class NoteList(generics.ListCreateAPIView):
+#     # context_object_name = "latest_notes_list"
+#     # template_name = "notes_api/index.html"
+#     # def get_queryset(self):
+#     queryset = Note.objects.all().order_by('-updated')
+#     serializer_class = NoteSerializer
+#     # return serializer_class
+
+
+
     # this function will return the notes list 
  
 # @permission_classes([IsAuthenticated])
-class NoteDetail(generics.RetrieveUpdateDestroyAPIView):
+class NoteDetail(generic.DetailView):
     queryset = Note.objects.all()
-    serializer_class = NoteSerializer
+    context_object_name = 'note_detail'
+    # serializer_class = NoteSerializer
+    template_name = "notes_api/detail.html"
+# class NoteDetail(generic.RetrieveUpdateDestroyAPIView):
+#     queryset = Note.objects.all()
+#     serializer_class = NoteSerializer
