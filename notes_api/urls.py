565f5b40b371457350b09fdcc79ec475b3a25532
Andy Wu <andyww09@gmail.com>
2023-10-13 22:33:03 -0700

    bug with login

--- notes_api/urls.py
+++ notes_api/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path 
+from django.contrib.auth.views import LogoutView
+from django.urls import path, include
 
 from . import views 
 app_name = 'notes_api'
 urlpatterns = [
     path('', views.NoteList.as_view(), name='note_list'),
+    path('accounts/', include('allauth.urls')),
+    path('logout', LogoutView.as_view(), name='logout'),
     path('detail/<int:pk>/', views.NoteDetail.as_view(), name='note_detail'),
     path('add_note/', views.add_note, name='add_note'),
     path('delete_note/<int:pk>/', views.delete_note, name='delete_note'),
