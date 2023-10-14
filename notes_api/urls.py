874891d29465c340b7e2a965c7e0f57b41e17292
Andy Wu <andyww09@gmail.com>
2023-10-13 17:04:14 -0700

    add functionality for editing notes

--- notes_api/urls.py
+++ notes_api/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'notes_api'
 urlpatterns = [
     path('', views.NoteList.as_view(), name='note_list'),
-    path('<int:pk>/', views.NoteDetail.as_view(), name='note_detail'),
+    path('detail/<int:pk>/', views.NoteDetail.as_view(), name='note_detail'),
     path('add_note/', views.add_note, name='add_note'),
+    path('edit/<int:pk>/', views.EditNote.as_view(), name='edit_note'),
+    path('edit/<int:pk>/update_note', views.update_note, name='update_note'),
 ]
--- notes_api/views.py
+++ notes_api/views.py
@@ -27,11 +27,22 @@ class NoteDetail(generic.DetailView):
     context_object_name = 'note_detail'
     template_name = "notes_api/detail.html"
 
+class EditNote(generic.DetailView):
+    queryset = Note.objects.all()
+    context_object_name = 'note_detail'
+    template_name = "notes_api/edit.html"
+
+def update_note(request, pk):
+    print("request", request)
+    if request.method=="POST":
+        note = NoteForm(request.POST)
+        if note.is_valid():
+            note.save()
+            return HttpResponseRedirect(reverse('notes_api:note_list'))
 
 def add_note(request):
     if request.method=="POST":
         note = NoteForm(request.POST)
-        # note = NoteForm(request.POST)
         if note.is_valid():
             note.save()
             return HttpResponseRedirect(reverse('notes_api:note_list'))
